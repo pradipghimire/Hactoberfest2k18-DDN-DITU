2e718761e222d4493d57eeacbce81cea393dc396
Sanyam Agarwal <sanyam99ag@gmail.com>
2018-10-18 00:17:39 +0530

    add pallindrome code

---
+++ Basics/Palindrome/pallindrome_Sanyam.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n, reverse = 0, remainder, original;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    original = n;
+ 
+    while( n!=0 )
+    {
+        remainder = n%10;
+        reverse = reverse*10 + remainder;
+        n /= 10;
+    }
+
+    if (original == reverse)
+        printf("%d is a palindrome.", original);
+    else
+        printf("%d is not a palindrome.", original);
+    
+    return 0;
+}
